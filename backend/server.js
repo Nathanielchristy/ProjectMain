b2656ce2803038f848e5bd12078072268ef6dce0
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2022-03-14 17:40:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 
-var cors = require('cors')
-app.use(cors())
+var cors = require("cors");
+app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/event", eventRoutes);

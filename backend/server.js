6b14052bbb62a60c5ce31ea0223c1f267baebaa6
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2022-03-14 17:32:46 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 
+var cors = require('cors')
+app.use(cors())
+
 app.use("/api/users", userRoutes);
 app.use("/api/event", eventRoutes);
 app.use("/api/timeslot", timeslotRoutes);
@@ -20,28 +23,27 @@ app.use(errorHandler);
 app.use(notFound);
 
 // --------------------------deployment------------------------------
-//  __dirname = path.resolve();
+__dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+// app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-})
+// // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+// })
 // --------------------------deployment------------------------------
 
 const PORT = process.env.PORT || 5000;

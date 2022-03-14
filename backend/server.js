2df07e877006376e250568b6719263808b9b7f05
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2022-03-14 17:46:17 +0530

    Final fix
    Final Fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const timeslotRoutes = require("./routes/timeslotRoutes");
-const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const {  errorHandler } = require("./middlewares/errorMiddleware");
 const path = require("path");
 
 const app = express();
@@ -20,15 +20,14 @@ app.use("/api/event", eventRoutes);
 app.use("/api/timeslot", timeslotRoutes);
 
 app.use(errorHandler);
-app.use(notFound);
+
 
 // --------------------------deployment------------------------------
 __dirname = path.resolve();
-
+console.log(__dirname);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
+  app.get("/", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
@@ -45,6 +44,12 @@ if (process.env.NODE_ENV === "production") {
 //   res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
 // })
 // --------------------------deployment------------------------------
+
+
+
+// app.get("/", (req, res) => {
+//   res.send("API is running..");
+// });
 
 const PORT = process.env.PORT || 5000;
 
